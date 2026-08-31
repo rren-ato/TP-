849dd788213bc63a014269de327b46e3a6bc1b26
rren-ato <renato.ruiz@pucp.edu.pe>
2026-08-31 00:23:59 -0500

    Add files via upload

---
+++ Lab 01/2026-1 (Tiene mas nivel algunos labs)/Resolucion_LAB1_2026-1/main.cpp
@@ -0,0 +1,17 @@
+#include "Bibliotecas/FuncionesAuxiliares.h"
+
+//Alumno:
+//Codigo:
+//Descripcion: Impresion de registros medicos por dia; hecho a traves de iterativas que filtran un rango de fechas
+//Se emite una estadistica, respecto a valores que superan al estandar,
+
+
+int main() {
+
+    reporte_final();
+// Primero se debe salir del PS - osea powershell
+    // Para ello debes colocar en la terminal: "cmd" (sin comillas) y luego colocar las rutas como en este ejemplo
+ // .\cm\Nombre_Archivo.exe < .\cm\ArchivosDeDatos\ClinicaDeUrgencias > .\cm\ArchivosDeReporte\reporte.txt
+ // al usar cm, luego de cada cm usar  TABULADOR (autocompleta)
+    return 0;
+}
