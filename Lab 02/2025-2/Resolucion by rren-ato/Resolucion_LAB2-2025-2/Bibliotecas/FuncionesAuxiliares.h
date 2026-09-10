70bb043d952fcd5eae7ddb7acdc8925e5997f6cb
rren-ato <renato.ruiz@pucp.edu.pe>
2026-09-10 18:15:24 -0500

    Add files via upload

---
+++ Lab 02/2025-2/Resolucion by rren-ato/Resolucion_LAB2-2025-2/Bibliotecas/FuncionesAuxiliares.h
@@ -0,0 +1,26 @@
+//
+// Created by Renato on 10/09/2026.
+//
+
+#ifndef RESOLUCION_LAB2_2025_2_FUNCIONESAUXILIARES_H
+#define RESOLUCION_LAB2_2025_2_FUNCIONESAUXILIARES_H
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+#define ANCHO_REPORTE 200
+#define ANCHO_REPORTE_JUSTIFICADO 194
+#define N_COLUMNS 6
+#define FOUND 1
+#define NOT_FOUND -1 // para busquedas a futuro (mecanisar)
+
+//Funciones
+
+
+
+#endif //RESOLUCION_LAB2_2025_2_FUNCIONESAUXILIARES_H
+
+
---
+++ Lab 02/2025-2/Resolucion by rren-ato/Resolucion_LAB2-2025-2/main.cpp
@@ -0,0 +1,6 @@
+#include "Bibliotecas/FuncionesAuxiliares.h"
+
+int main() {
+
+    return 0;
+}
