4f0506a699b2a97c788d4cf1283f7de6368b54ca
rren-ato <renato.ruiz@pucp.edu.pe>
2026-09-11 02:26:39 -0500

    Add files via upload

---
+++ Lab 02/2025-2/Resolucion_LAB2-2025-2/main.cpp
@@ -0,0 +1,9 @@
+#include "Bibliotecas/FuncionesAuxiliares.h"
+
+//* RECORDAR DESCARGAR EN PLUGLINS BETTER COMMENTS PARA VER MEJOR LOS COMENTARIOS (COLORES Y ESO)
+int main() {
+
+    // generar_reporte(const char *archivo_lectura_streams, const char *archivo_reporte)
+
+    return 0;
+}
