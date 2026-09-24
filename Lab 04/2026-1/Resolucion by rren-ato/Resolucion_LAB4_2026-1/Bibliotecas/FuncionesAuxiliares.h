1b9a54c00c52aa3ce9547b527b937b036d47d993
rren-ato <renato.ruiz@pucp.edu.pe>
2026-09-24 01:35:33 -0500

    Add files via upload

---
+++ Lab 04/2026-1/Resolucion by rren-ato/Resolucion_LAB4_2026-1/Bibliotecas/FuncionesAuxiliares.h
@@ -0,0 +1,55 @@
+//
+// Created by Renato on 23/09/2026.
+//
+
+#ifndef RESOLUCION_LAB4_2026_1_FUNCIONESAUXILIARES_H
+#define RESOLUCION_LAB4_2026_1_FUNCIONESAUXILIARES_H
+
+//Utils
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+#define MAX_PACIENTES 100
+#define MAX_ESPECIALIDADES 20
+#define MAX_TRIAJES 600
+#define NOT_FOUND -1
+
+//Funciones
+
+//! Evitar pensar asi
+//? void generar_reportes(const char* fileEspecialidades, const char* filePacientes, const char* fileTriaje,
+//                      const char* reporte);
+//* ES UNA ESTRUCTURA DISTINTA A LA DE BUSQUEDAS CON SEEKG (AYUDA A NO CONFUNDIRSE) <-- PARA EL PARCIAL
+
+//? Apertura de archivos
+void open_read_file(ifstream &input, const char* file_name);
+void open_write_file(ofstream &output, const char* file_name);
+
+//? Snipets
+void print_line(ofstream &output,int width, char character);
+void ignorar_coma(ifstream &input);
+int read_ID(ifstream &input);
+int read_date (ifstream &input);
+int read_time(ifstream &input);
+
+
+//? BUSQUEDAS (DE ARREGLOS)
+int buscarPaciente(int codigoPaciente, int *arrCodigosPaciente);
+int buscarEspecialidad(int codigoEspecialidad, int *arrCodigosEspecialidades);
+
+
+//? Lectura (llenado de arreglos)
+void llenar_pacientes(const char* filePacientes, int *codigosPacientes, int *edadesPacientes, char *sexosPacientes);
+void llenar_especialidades(const char* fileEspecialidades, int *codigosEspecialidades, double *costosEspecialidades);
+void llenar_triaje(const char* fileTriaje,int *CodigosPacientes,int *CodigosEspecialidades,double *costosEspecialidades,
+                   int *fechasTriajes, int *frecuenciasTriajes, int *presionesSisTriajes, int *presionesDiasTriajes,
+                   double *temperaturaMaxTriajes, double *CostoTotal);
+
+//? Impresion
+
+
+
+
+#endif //RESOLUCION_LAB4_2026_1_FUNCIONESAUXILIARES_H
---
+++ Lab 04/2026-1/Resolucion by rren-ato/Resolucion_LAB4_2026-1/main.cpp
@@ -0,0 +1,34 @@
+#include "Bibliotecas/FuncionesAuxiliares.h"
+
+int main() {
+
+    //? LECTURA Y LLENADO DE ARREGLOS (REVISAR TEORIA DE ESTO)
+    //Segun erasmo esto era informalidad y debia ir en el UTILS
+    // #define MAX_PACIENTE 100
+
+    int arrCodigoPaciente[MAX_PACIENTES];
+    int arrEdadPaciente[MAX_PACIENTES];
+    char arrSexoPaciente[MAX_PACIENTES];
+    llenar_pacientes("ArchivosDeDatos/Pacientes_TP_L4.json", arrCodigoPaciente, arrEdadPaciente, arrSexoPaciente);
+
+    int arrCodigoEspecialidad[MAX_ESPECIALIDADES];
+    double arrCostoEspecialidad[MAX_ESPECIALIDADES];
+    llenar_especialidades("ArchivosDeDatos/Especialidades_TP_L4.json", arrCodigoEspecialidad, arrCostoEspecialidad);
+
+    int arrFechaTriaje[MAX_TRIAJES];
+    int arrfrecuenciaTriaje[MAX_TRIAJES];
+    int arrpresionSisTriaje[MAX_TRIAJES];
+    int arrpresionDiasTriaje[MAX_TRIAJES];
+    double arrtemperaturaMaxTriaje[MAX_TRIAJES];
+    double arrCostoTotal[MAX_TRIAJES];
+
+    llenar_triaje("ArchivosDeDatos/Triaje_TP_L4.csv", arrCodigoPaciente, arrCodigoEspecialidad, arrCostoEspecialidad,
+                   arrFechaTriaje, arrfrecuenciaTriaje, arrpresionSisTriaje, arrpresionDiasTriaje,
+                   arrtemperaturaMaxTriaje, arrCostoTotal);
+
+    //? IMPRESION
+
+
+
+    return 0;
+}
