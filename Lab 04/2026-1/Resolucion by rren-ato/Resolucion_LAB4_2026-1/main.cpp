c44b8bb745edcc648e9f2fb8738451e299a832d0
rren-ato <renato.ruiz@pucp.edu.pe>
2026-09-24 20:19:39 -0500

    Add patient and specialty count parameters to functions

--- Lab 04/2026-1/Resolucion by rren-ato/Resolucion_LAB4_2026-1/main.cpp
+++ Lab 04/2026-1/Resolucion by rren-ato/Resolucion_LAB4_2026-1/main.cpp
@@ -9,11 +9,16 @@ int main() {
     int arrCodigoPaciente[MAX_PACIENTES];
     int arrEdadPaciente[MAX_PACIENTES];
     char arrSexoPaciente[MAX_PACIENTES];
-    llenar_pacientes("ArchivosDeDatos/Pacientes_TP_L4.json", arrCodigoPaciente, arrEdadPaciente, arrSexoPaciente);
+    int cantPacientes = 0; // <--- ESTO ES LA LONGITUD QUE SE LLENA EN EL ARREGLO
+    //* RECORDAR QUE ES CLAVE DIFERENCIAR ENTRE LA CAPACIDAD Y LONGITUD DE UN ARREGLO (TEORIA)
+    llenar_pacientes("ArchivosDeDatos/Pacientes_TP_L4.json", arrCodigoPaciente, arrEdadPaciente, arrSexoPaciente,
+                      cantPacientes);
 
     int arrCodigoEspecialidad[MAX_ESPECIALIDADES];
     double arrCostoEspecialidad[MAX_ESPECIALIDADES];
-    llenar_especialidades("ArchivosDeDatos/Especialidades_TP_L4.json", arrCodigoEspecialidad, arrCostoEspecialidad);
+    int cantEspecialidades = 0;
+    llenar_especialidades("ArchivosDeDatos/Especialidades_TP_L4.json", arrCodigoEspecialidad, arrCostoEspecialidad,
+                          cantEspecialidades);
 
     int arrFechaTriaje[MAX_TRIAJES];
     int arrfrecuenciaTriaje[MAX_TRIAJES];
@@ -27,6 +32,9 @@ int main() {
                    arrtemperaturaMaxTriaje, arrCostoTotal);
 
     //? IMPRESION
+    imprimirReporte("ArchivosDeReporte/reporte_final.txt", arrCodigoPaciente, arrEdadPaciente, arrSexoPaciente,
+                     arrFechaTriaje, arrfrecuenciaTriaje, arrpresionSisTriaje, arrpresionDiasTriaje,
+                     arrtemperaturaMaxTriaje, arrCostoTotal);
 
 
 
